0683fb6fc72444d30bc31f52f02b7bbb23e527eb
Steven McCombe <stevenmccombe93@gmail.com>
2023-04-24 18:16:26 -0400

    added functionality with chat gpt to return responses

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [ingredients, setIngredients] = useState('');
+  const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchCocktail = async () => {
+    setLoading(true);
+    try {
+      const prompt = `create a unique cocktail that has never been made before using any of the following ingredients: ${ingredients}. Give the cocktail a unique name.`;
+      const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
+  prompt,
+  max_tokens: 100,
+  n: 1,
+  stop: null,
+  temperature: 0.8,
+}, {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+  },
+});
+
+      const generatedRecipe = response.data.choices[0].text.trim();
+      setRecipe(generatedRecipe);
+    } catch (error) {
+      console.error('Error fetching cocktail recipe:', error);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Unique Cocktail Generator</h1>
+      <label htmlFor="ingredients">Enter ingredients:</label>
+      <input
+        type="text"
+        id="ingredients"
+        value={ingredients}
+        onChange={(e) => setIngredients(e.target.value)}
+        placeholder="e.g. vodka, orange juice, grenadine"
+      />
+      <button onClick={fetchCocktail} disabled={loading}>
+        {loading ? 'Loading...' : 'Find a Cocktail'}
+      </button>
+      {recipe && (
+        <div>
+          <h2>Generated Cocktail Recipe:</h2>
+          <p>{recipe}</p>
+        </div>
+      )}
     </div>
   );
 }
