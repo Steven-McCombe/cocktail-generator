2976947c75971080cf2c825c3309edfd19ea4cfe
Steven McCombe <stevenmccombe93@gmail.com>
2023-04-24 19:48:25 -0400

    adjusted the temp, refined prompt

--- src/App.js
+++ src/App.js
@@ -4,26 +4,47 @@ import Select from 'react-select';
 import './App.css';
 
 const categories = {
-  liquors: [
+  spirits: [
     { value: 'vodka', label: 'Vodka' },
     { value: 'gin', label: 'Gin' },
     { value: 'rum', label: 'Rum' },
+    { value: 'tequila', label: 'Tequila' },
+    { value: 'whiskey', label: 'Whiskey' },
   ],
-  whiskeys: [
-    { value: 'scotch', label: 'Scotch' },
-    { value: 'bourbon', label: 'Bourbon' },
+  liqueurs: [
+    { value: 'triple sec', label: 'Triple Sec' },
+    { value: 'kahlua', label: 'Kahlua' },
+    { value: 'baileys', label: "Bailey's Irish Cream" },
+  ],
+  wines: [
+    { value: 'red wine', label: 'Red Wine' },
+    { value: 'white wine', label: 'White Wine' },
+    { value: 'prosecco', label: 'Prosecco' },
+    { value: 'champagne', label: 'Champagne' },
+  ],
+  beers: [
+    { value: 'lager', label: 'Lager' },
+    { value: 'ale', label: 'Ale' },
+    { value: 'cider', label: 'Cider' },
   ],
   mixers: [
     { value: 'tonic water', label: 'Tonic Water' },
-    { value: 'orange juice', label: 'Orange Juice' },
-  ],
-  herbs: [
-    { value: 'mint', label: 'Mint' },
-    { value: 'basil', label: 'Basil' },
+    { value: 'cola', label: 'Cola' },
+    { value: 'lemonade', label: 'Lemonade' },
+    { value: 'ginger ale', label: 'Ginger Ale' },
   ],
   fruits: [
     { value: 'lemon', label: 'Lemon' },
     { value: 'lime', label: 'Lime' },
+    { value: 'orange', label: 'Orange' },
+    { value: 'pineapple', label: 'Pineapple' },
+    { value: 'strawberry', label: 'Strawberry' },
+  ],
+  herbs: [
+    { value: 'mint', label: 'Mint' },
+    { value: 'basil', label: 'Basil' },
+    { value: 'rosemary', label: 'Rosemary' },
+    { value: 'thyme', label: 'Thyme' },
   ],
 };
 
@@ -47,13 +68,13 @@ function App() {
     setLoading(true);
     const ingredients = selectedIngredients.map(ingredient => ingredient.value).join(', ');
     try {
-      const prompt = `Generate a unique cocktail recipe using the following ingredients: ${ingredients}. Respond with the following format: Name, Ingredients:List , Instructions: list`;
+      const prompt = `Generate a cocktail recipe using any of the following ingredients(Remember you don't have to use all of the, bonus points if you create something unique. You can assume i have some general household ingredients like milk, coffee, pepper etc.): ${ingredients}. Respond with the following format: Name:, Ingredients:, Instructions:. You should also take into consideration dangerous ingredients that are not fit for human consumption and ignore them. Include measurements for all ingredients`;
       const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-002/completions', {
         prompt,
         max_tokens: 200,
         n: 1,
         stop: null,
-        temperature: 1.0,
+        temperature: 0.8,
       }, {
         headers: {
           'Content-Type': 'application/json',
@@ -62,13 +83,14 @@ function App() {
       });
   
       const rawGeneratedRecipe = response.data.choices[0].text.trim();
-      const formattedRecipe = rawGeneratedRecipe.split(',').map((part, index) => {
+      const parts = rawGeneratedRecipe.split(/, (?=[a-zA-Z]+:)/); // Split on commas followed by a word and a colon
+      const formattedRecipe = parts.map((part, index) => {
         if (index === 0) {
           return `<b>Name:</b> ${part}`;
         } else if (index === 1) {
-          return `<b>Ingredients:</b>${part.replace('Ingredients:', '')}`;
+          return `<b>Ingredients:</b>${part.replace('Ingredients:', '').replace(/, /g, '<br/>')}`;
         } else if (index === 2) {
-          return `<b>Instructions:</b>${part.replace('Instructions:', '')}`;
+          return `<b>Instructions:</b>${part.replace('Instructions:', '').replace(/, /g, '<br/>')}`;
         }
         return part;
       }).join('<br/>');
@@ -82,9 +104,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Unique Cocktail Generator</h1>
+    <h1>Unique Cocktail Generator</h1>
+    <div className="ingredient-grid">
       {Object.entries(categories).map(([category, options]) => (
-        <div key={category}>
+        <div key={category} className="ingredient-category">
           <label htmlFor={`${category}-ingredients`}>{category.charAt(0).toUpperCase() + category.slice(1)}:</label>
           <Select
             isMulti
@@ -100,6 +123,7 @@ function App() {
           />
         </div>
       ))}
+    </div>
       <div>
         <label htmlFor="custom-ingredient">Custom Ingredient:</label>
         <input
